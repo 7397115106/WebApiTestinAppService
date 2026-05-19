3ed416e4c99cec614f3f4dc28ce1cfacaeb66dd8
Harish <harish@gmail.com>
2026-05-19 17:33:38 +0530

    Initial commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_My_New_Webapi_application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = new List<string>
+            {
+                "Harish",
+                "Ravi",
+                "Kiran"
+            };
+
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            return Ok($"Employee Id: {id}");
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(string name)
+        {
+            return Ok($"Employee Added: {name}");
+        }
+    }
+}
